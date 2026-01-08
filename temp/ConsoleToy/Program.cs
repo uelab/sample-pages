828d8279fcd1970fdf973a98d9e022b9ad74530c
alt-thompson <anne.loomis@outlook.com>
2026-01-08 11:18:31 -0800

    Add temp files with data for papers

---
+++ temp/ConsoleToy/Program.cs
@@ -0,0 +1,33 @@
+﻿//FileStream fs = File.Open(@"C:\src\sample-pages\temp\paper.json", FileMode.Open, FileAccess.Read);
+
+using Azure;
+using Azure.Core.Serialization;
+
+byte[] bytes = File.ReadAllBytes(@"C:\src\sample-pages\temp\paper.json");
+BinaryData data = new(bytes);
+dynamic json = data.ToDynamicFromJson(JsonPropertyNames.CamelCase);
+dynamic appsWarmupData = json.appsWarmupData;
+dynamic dataBinding = appsWarmupData.dataBinding;
+dynamic dataStore = dataBinding.dataStore;
+dynamic records = dataStore.recordsByCollectionId;
+
+foreach (dynamic collection in records)
+{
+    Console.WriteLine("***" + (string)collection.Name);
+    Console.WriteLine();
+
+    foreach (dynamic paper in collection.Value)
+    {
+        Console.WriteLine((string)paper.Value.title);
+
+        string url = paper.Value.url;
+        string link = paper.Value.link;
+
+        string loc = url == null ? link : url;
+
+        Console.WriteLine(loc);
+        Console.WriteLine();
+    }
+}
+
+Console.ReadLine();
