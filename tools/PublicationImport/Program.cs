b0291dd72330648d8ed396f0e6e161e393218f8e
alt-thompson <anne.loomis@outlook.com>
2026-01-27 10:20:19 -0800

    pull more data fields before parsing data from title

--- tools/PublicationImport/Program.cs
+++ tools/PublicationImport/Program.cs
@@ -3,7 +3,7 @@
 using Azure;
 using Azure.Core.Serialization;
 
-byte[] bytes = File.ReadAllBytes(@"C:\src\sample-pages\temp\paper.json");
+byte[] bytes = File.ReadAllBytes(@"C:\src\sample-pages\tools\temp\paper.json");
 BinaryData data = new(bytes);
 dynamic json = data.ToDynamicFromJson(JsonPropertyNames.CamelCase);
 dynamic appsWarmupData = json.appsWarmupData;
@@ -18,14 +18,20 @@ foreach (dynamic collection in records)
 
     foreach (dynamic paper in collection.Value)
     {
-        Console.WriteLine((string)paper.Value.title);
+        Console.WriteLine("Title: " + (string)paper.Value.title);
+        Console.WriteLine("Award: " + (string)paper.Value.award);
 
         string url = paper.Value.url;
         string link = paper.Value.link;
-
         string loc = url == null ? link : url;
 
-        Console.WriteLine(loc);
+        Console.WriteLine("Link: " + loc);
+
+        string paperImage = paper.Value.paperImage;
+        string image = paper.Value.image;
+        string img = paperImage == null ? image : paperImage;
+
+        Console.WriteLine("Image: " + img);
         Console.WriteLine();
     }
 }
